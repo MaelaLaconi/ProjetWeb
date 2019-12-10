12cad77ab05216c8799fab1224467a3166199237
MaelaLaconi <44729941+MaelaLaconi@users.noreply.github.com>
2019-12-10 15:42:59 +0100

    Add files via upload

--- Acces_hierarchique.php
+++ Acces_hierarchique.php
@@ -1,11 +1,13 @@
 
 <?php
     include 'Donnees.inc.php' ;
-    //include 'InstallBddProjet.php' ;
+    include 'InstallBddProjet.php' ;
+	    //require_once('InstallBddProjet.php');
 
+	$mysqli = creerBase();
     //tableau de recette
-    
-   /* echo "<table border=2>" ;
+   /* echo "tableau recette : " ;
+    echo "<table border=2>" ;
     echo "<tr>" ;
     echo "<td>id</td>" ;
     echo "<td>titre</td>" ;
@@ -17,24 +19,57 @@
     foreach($Recettes as $numero => $titre){  //clef -> valeure
         echo "<tr>" ;
 
-        echo "<td>".$numero."</td>" ;
+        echo "<td> premier".$numero."</td>" ;
 
         foreach($titre as $v1 => $v2){ 
             if(!is_array($v2)){
-                echo "<td>".$v2."</td>" ;
+				//insertRecette($base,$mysqli,$numero,$titre,$ingredient,$preparation,$index);
+
+                echo "<td> deuxieme".$v2."</td>" ;
             }
             else{
                 $tmp=implode(",",$v2) ;
-                echo "<td>".$tmp."</td>" ;
+                echo "<td> troisieme".$tmp."</td>" ;
             }
         }
         echo "</tr>" ;
     }
-    echo "</table>" ;
+    echo "</table>" ;*/
 
+	//***********************************
+	/* 0 => 
+  array (
+    'titre' => 'Alerte à Malibu (Boisson de la couleurs des fameux maillots de bains... ou presque)',
+    'ingredients' => '50 cl de malibu coco|50 cl de gloss cerise|1 l de jus de goyave blanche|1 poignée de griottes',
+    'preparation' => 'Mélanger tous les ingrédients ensemble dans un grand pichet. Placer au frais au moins 3 heures avant de déguster. Tchin tchin !!',
+    'index' => 
+    array (
+      0 => 'Malibu',
+      1 => 'Cerise',
+      2 => 'Jus de goyave',
+      3 => 'Cerise griotte',
+    ),
+  ),*/
+	   foreach($Recettes as $clef1 => $clef2){  //clef2 = tab( titre, ing, prep, index[])
+			foreach($clef2['index'] as $clef3 => $clef4){
+				 insertRecette("boisson",$mysqli,$clef1,$clef2['titre'],$clef2['ingredients'],$clef2['preparation'],$clef4);
+			}
+	   }
+	
+	$result = afficheRecette($base,$mysqli,$id);
+	//echo $result;
+	
+	while($row = mysqli_fetch_array($result)){
+	    echo $row['id'] . "</br>";
+	    echo $row['titre'] . "</br>";
+	    echo $row['ingredients'] . "</br>";
+	    echo $row['preparation'] . "</br>";
+	    echo $row['listeCategorie'] . "</br>";
+	}
+	//**************************************
 
     //tableau de hierarchie
-    echo "<table border=2>" ;
+    /*echo "<table border=2>" ;
     echo "<tr>" ;
     echo "<td>categorie</td>" ;
     echo "<td>sous_cat</td>" ;
@@ -83,21 +118,22 @@
     var tab = new Array() ;
     console.log("Dans la fonction javascript") ;
     <?php 
-    foreach($Hierarchie as $cle1 => $cat){  //clef -> valeure
+    /*foreach($Hierarchie as $cle1 => $cat){  //clef -> valeure
         foreach($cat['super-categorie'] as $cle2 => $sous_cat){
             
             if(!is_array($sous_cat)){
-                echo 'tab.push("'.$sous_cat.'");';
-
+					echo 'tab.push("'.$sous_cat.'");';
+				
             }
             else{
                 foreach($sous_cat as $clef3 => $clef4){
-                    echo 'tab.push("'.$clef4.'");';
 
+						echo 'tab.push("'.$clef4.'");';
+					
                 }
             }
         }
-    }
+    }*/
    ?>;
    
     str="<select name='aliments' onChange='choixCateg()'>" ;
@@ -120,7 +156,7 @@
             foreach($Hierarchie as $cle1 => $cat){  //clef -> valeure
                     echo 'tabCateg.push("'.$cle1.'");';
                     console.log("Dans le for") ;
-        }
+			}
         ?>
         
         tmp="<select name='categ' onChange='choixSousCateg()'>" ;
--- InstallBddProjet.php
+++ InstallBddProjet.php
@@ -1,13 +1,14 @@
 
 <?php
 
+	// Fait les requetes
     function query($link,$requete){
  		$resultat=mysqli_query($link,$requete) or die("$requete :".mysqli_error($link));
  	    return($resultat);
 
  	}
 
-
+	// Ajoute un ingredient
  	function insertIngredient($base,$link,$categorie,$souscategorie,$supercategorie){
  		$requete = "USE $base;INSERT INTO Ingredient VALUES( $categorie ,$souscategorie,$supercategorie)";
  		
@@ -16,13 +17,14 @@
 		}
  	}
 
+	// Ajoute une recette
  	function insertRecette($base,$link,$id,$titre,$ingredient,$preparation,$index){
+		echo "dans l'insert" ;
  		$requete = "USE $base;INSERT INTO Recette VALUES( $id,$titre,$ingredient,$preparation,$index)";
  		$resultat;
 		foreach(explode(';',$requete) as $recette) {
 			$resultat = query($link,$recette);
 		}
-
 		
  	}
 
@@ -46,15 +48,12 @@
 		return($resultat);
  	}
 
+	function destruction(){
+		mysqli_close($mysqli); 
+	}
     function creerBase(){
-    
-    
-    
-    
-    }
-
- 	$mysqli=mysqli_connect('127.0.0.1', 'root', '') or die("Erreur de connexion");
- 	$base="Boisson";
+    $mysqli=mysqli_connect('127.0.0.1', 'root', '') or die("Erreur de connexion");
+ 	$base="boisson";
  	$Sql="
 	 DROP DATABASE IF EXISTS $base;
 	 CREATE DATABASE $base;
@@ -68,37 +67,12 @@
 	foreach(explode(';',$Sql) as $Requete) {
 		query($mysqli,$Requete);
 	}
+	return $mysqli ;
+	echo "table creer" ;
+    
+    }
 
+ 	
 
-	//Test pour insert ingredient
-	 $categorie = "'test2'";
-	 $souscategorie = "'test'";
-	 $supercategorie = "'test'";
-
-	insertIngredient($base,$mysqli,$categorie,$souscategorie,$supercategorie);
-
-
-	//Test pour insert recette
-	 $id = "2";
-	 $titre = "'Alerte à Malibu (Boisson de la couleurs des fameux maillots de bains... ou presque)'";
-	 $ingredient = "'50 cl de malibu coco|50 cl de gloss cerise|1 l de jus de goyave blanche|1 poignée de griottes'";
-	 $preparation = "'Mélanger tous les ingrédients ensemble dans un grand pichet. Placer au frais au moins 3 heures avant de déguster. Tchin tchin !!'";
-	 $index ="'Malibu,Cerise,Jus de goyave,Cerise griotte'";
-	insertRecette($base,$mysqli,$id,$titre,$ingredient,$preparation,$index);
-	
-
-	/*Test affichage d'une recette*/
-	$result = afficheRecette($base,$mysqli,$id);
-	//echo $result;
-	
-	while($row = mysqli_fetch_array($result)){
-	    echo $row['id'] . "</br>";
-	    echo $row['titre'] . "</br>";
-	    echo $row['ingredients'] . "</br>";
-	    echo $row['preparation'] . "</br>";
-	    echo $row['listeCategorie'] . "</br>";
-	}
-
-	mysqli_close($mysqli); 
 
 ?>
